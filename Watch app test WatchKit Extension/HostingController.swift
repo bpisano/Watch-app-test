5dec9654c1c2b5f18ed400f685f1169c5ff445ff
Benjamin Pisano <benjamin.pisano@icloud.com>
2020-05-29 14:53:28 +0200

    Initial Commit

---
+++ Watch app test WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Watch app test WatchKit Extension
+//
+//  Created by Benjamin Pisano on 29/05/2020.
+//  Copyright © 2020 Snopia. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
